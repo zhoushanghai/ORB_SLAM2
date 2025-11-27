b8fdc9c04a5969494ff89f471b2edbaf0d96429f
wanghaizhou <whz@whz.com>
2025-11-27 15:49:55 +0800

    搭好环境

--- include/System.h
+++ include/System.h
@@ -25,6 +25,8 @@
 #include<string>
 #include<thread>
 #include<opencv2/core/core.hpp>
+#include <unistd.h>
+
 
 #include "Tracking.h"
 #include "FrameDrawer.h"
@@ -35,6 +37,7 @@
 #include "KeyFrameDatabase.h"
 #include "ORBVocabulary.h"
 #include "Viewer.h"
+
 
 namespace ORB_SLAM2
 {
